512a89295864e989275abb706d97747ec39ebbe2
Adam Skoczylas <skocadam@gmail.com>
2018-12-13 20:00:51 -0500

    set up app components

---
+++ src/components/SkiDayCount.js
@@ -0,0 +1,35 @@
+import '../stylesheets/ui.scss'
+import {MdTerrain} from 'react-icons/md'
+import {TiWeatherSnow} from 'react-icons/ti'
+import {FaCalendarAlt} from 'react-icons/fa'
+
+const percentToDecimal = (decimal) => {
+    return `${decimal * 100}%`
+}
+
+const calculateGoalProgress = (total, goal) => {
+    return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+    <div className="ski-day-count">
+        <div className="total-days">
+            <span>{total}</span>
+                <FaCalendarAlt />
+            <span>days</span>
+        </div>
+        <div className="powder-days">
+            <span>{powder}</span>
+                <TiWeatherSnow />
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{backcountry}</span>
+                <MdTerrain />
+            <span>days</span>
+        </div>
+        <div className="goal">
+            <span>{calculateGoalProgress(total, goal)}</span>
+        </div>
+    </div>
+)
---
+++ src/components/SkiDayRow.js
@@ -0,0 +1,4 @@
+import { MdTerrain } from 'react-icons/md'
+import { TiWeatherSnow } from 'react-icons/ti'
+import { FaCalendarAlt } from 'react-icons/fa'
+
