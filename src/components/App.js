df3e6afa887b8fd65a9aedd3c7194580ddc7cd16
Adam Skoczylas <skocadam@gmail.com>
2018-12-17 11:17:00 -0500

    add filter to table

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,8 @@ export const App = createClass({
                         backcountry={this.countDays("backcountry")} /> :
                     (this.props.location.pathname === "/add-day") ?
                         <AddDayForm /> :
-                        <SkiDayList days={this.state.allSkiDays} />
+                        <SkiDayList days={this.state.allSkiDays}
+                            filter={this.props.params.filter} />
                 }
             </div>
         )
--- src/components/SkiDayList.js
+++ src/components/SkiDayList.js
@@ -1,26 +1,45 @@
 import { SkiDayRow } from './SkiDayRow'
-import { PropTypes } from 'react'
+import { Link } from 'react-router'
 
-export const SkiDayList = ({days}) => (
-    <table>
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Resort</th>
-                <th>Powder</th>
-                <th>Backcounry</th>
-            </tr>
-        </thead>
-        <tbody>
-            {days.map((day, i) => 
-                <SkiDayRow key={i} {...day} />
-            )}
-        </tbody>
-    </table>
-)
+export const SkiDayList = ({ days, filter }) => {
+    const filteredDays = (!filter || !filter.match(/powder||backcountry/)) ? days : days.filter(day => day[filter])
+
+    return (
+        <div className="ski-day-list">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Resort</th>
+                        <th>Powder</th>
+                        <th>Backcounry</th>
+                    </tr>
+                    <tr>
+                        <td colSpan={4}>
+                            <Link to="list-days">
+                                All days
+                            </Link>
+                            <Link to="list-days/powder">
+                                Powder Days
+                            </Link>
+                            <Link to="list-days/backcountry">
+                                Backcountry
+                            </Link>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredDays.map((day, i) =>
+                        <SkiDayRow key={i} {...day} />
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
 
 SkiDayList.propTypes = {
-    days: function(props) {
+    days: function (props) {
         if (!Array.isArray(props.days)) {
             return new Error(
                 "SkiDayList should be an array"
