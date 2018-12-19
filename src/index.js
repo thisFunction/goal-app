a33201c8d50878b68e9ee54cf63170bdd20ce959
Adam Skoczylas <skocadam@gmail.com>
2018-12-19 11:05:17 -0500

    add constants, initial state and reducers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { App } from './components/App'
-import { Whoops404 } from './components/Whoops404'
-import { Router, Route, hashHistory } from 'react-router'
-import { AddDayForm } from './components/AddDayForm';
-window.React = React;
+// import React from 'react'
+// import { render } from 'react-dom'
+// import { App } from './components/App'
+// import { Whoops404 } from './components/Whoops404'
+// import { Router, Route, hashHistory } from 'react-router'
+// import { AddDayForm } from './components/AddDayForm';
+// window.React = React;
 
-render(
-	<Router history={hashHistory}>
-		<Route path="/" component={App}/>
-		<Route path="/list-days" component={App}>
-			<Route path=":filter" component={App}/>
-		</Route>
-		<Route path="/add-day" component={App}/>
-		<Route path="*" component={Whoops404}/>
-	</Router>,
-	document.getElementById('react-container')
-)
+// render(
+// 	<Router history={hashHistory}>
+// 		<Route path="/" component={App}/>
+// 		<Route path="/list-days" component={App}>
+// 			<Route path=":filter" component={App}/>
+// 		</Route>
+// 		<Route path="/add-day" component={App}/>
+// 		<Route path="*" component={Whoops404}/>
+// 	</Router>,
+// 	document.getElementById('react-container')
+// )
+
+import C from './constants'
+import {allSkiDays } from './store/reducers'
+
+const state = [
+	{
+		"resort" : "Kirkwood",
+		"date" : "2012-2-5",
+		"powder" : true,
+		"backcountry" : false
+	},
+	{
+		"resort" : "Boreal",
+		"date" : "2016-12-15",
+		"powder" : true,
+		"backcountry" : true
+	}
+]
+
+const action = {
+	type: C.REMOVE_DAY,
+	payload: "2016-12-15"
+}
+
+const nextState = allSkiDays(state, action)
+
+console.log(`
+    initial state: ${JSON.stringify(state)}
+    action: ${JSON.stringify(action)}
+    new state: ${JSON.stringify(nextState)}
+`)
---
+++ src/store/reducers.js
@@ -0,0 +1,54 @@
+import constants from '../constants'
+
+export const goal = (state = 10, action) =>
+    (action.type === constants.SET_GOAL) ? parseInt(action.payload) : state
+
+export const skiDay = (state = null, action) =>
+    (action.type === constants.ADD_DAY) ? action.payload : state
+
+export const errors = (state = [], action) => {
+    switch (action.type) {
+        case constants.ADD_ERROR:
+            return [...state, action.payload]
+        case constants.CLEAR_ERROR:
+            return state.filter((message, i) => i !== action.payload)
+        default:
+            return state
+    }
+}
+
+export const allSkiDays = (state = [], action) => {
+    switch(action.type) {
+        case constants.ADD_DAY:
+            const hasDayAlready = state.some(skiDay => skiDay.date === action.payload.date)
+            return (hasDayAlready) ? state : [...state, action.payload]
+        case constants.REMOVE_DAY:
+            return state.filter(skiDay => skiDay.date !== action.payload)
+        default:
+            return state
+    }
+}
+
+export const fetching = (state = false, action) => {
+    switch (action.type) {
+        case constants.FETCH_RESORT_NAMES:
+            return true
+        case constants.CANCEL_FETCHING:
+            return false
+        case constants.CHANGE_SUGGESTION:
+            return false
+        default:
+            return state
+    }
+}
+
+export const suggestions = (state = [], action) => {
+    switch (action.type) {
+        case constants.CHANGE_SUGGESTIONS:
+            return action.payload
+        case constants.CLEAR_SUGGESTIONS:
+            return []
+        default:
+            return state
+    }
+}
